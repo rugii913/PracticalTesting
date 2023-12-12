ffc262b28f333f48f4426c5fa7d7ea1f7f3c08bb
rugii913 <rugii913@gmail.com>
2023-12-12 20:34:23 +0900

    TDD 3단계
    - 초록불을 유지하면서 구현부 리팩토링
    - 같은 기능이라는 것을 테스트가 보장해주고 있다.

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -44,7 +44,11 @@ public class CafeKiosk {
     }
 
     public int calculateTotalPrice() {
-        return 8500;
+        int totalPrice = 0;
+        for (Beverage beverage : beverages) {
+            totalPrice += beverage.getPrice();
+        }
+        return totalPrice;
     }
 
     public Order createOrder() {
