2879a8d74f4690515642e3b1bdd83c325b7796b2
rugii913 <rugii913@gmail.com>
2023-12-17 19:54:00 +0900

    주문에 대한 비즈니스 로직 짜기 전 controller, request, service 준비

---
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -0,0 +1,12 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+
+@Service
+public class OrderService {
+
+    public void createOrder(OrderCreateRequest request) {
+        
+    }
+}
