8d84d05ab5650a0469507b7029242f0795af785a
rugii913 <rugii913@gmail.com>
2023-12-17 20:53:04 +0900

    (red)OrderServiceTest 먼저 작성 - 당연히 테스트 실패

---
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -0,0 +1,68 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+
+ @SpringBootTest
+class OrderServiceTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private OrderService orderService; // 테스트 하고자 하는 orderService를 주입받는다. // 테스트에서는 그냥 필드 주입
+
+    @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrder() {
+        // given
+        Product product1 = createProduct(HANDMADE, "001", 1000);
+        Product product2 = createProduct(HANDMADE, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "002"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(LocalDateTime.now(), 4000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("002", 3000)
+                );
+    }
+
+    private Product createProduct(ProductType type, String productNumber, int price) { // product를 만들어주는 도우미 메서드, 테스트 코드에서 한 부분이 너무 길어지는 것을 방지
+        // 테스트에 필요한 부분만 신경 쓰고, 나머지는 임의의 값을 넣어둘 것
+        return Product.builder()
+                .type(type)
+                .productNumber(productNumber)
+                .price(price)
+                .sellingStatus(SELLING)
+                .name("메뉴 이름")
+                .build();
+    }
+}
