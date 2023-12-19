b3bb9a5b9102baaef802878a493c17876bd0b5ec
rugii913 <rugii913@gmail.com>
2023-12-19 22:29:12 +0900

    (에러 해결 및 수정)OrderCreateRequest 생성자 에러 해결, 수동 체크 위해 api 데이터 반환하도록 함
    - OrderCreateRequest 에러 해결
      - 기본 생성자를 만들어주었다.
        - 기존에는 @Builder 붙은 private 생성자만 있는 상태였음
        - jackson이 binding하지 못했음
      - presentation layer를 체크하지 않고 있다보니 누락된 부분이다.
    
    - OrderCreateRequest의 createOrder(~)가 데이터를 반환하도록 함
      - http 파일로 시험 시 반환 데이터를 수동으로도 체크해보기 위함

--- src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
@@ -2,10 +2,15 @@ package sample.cafekiosk.spring.api.controller.order.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+// Builder를 만들기 위해 private 해놓은 생성자만 있고, 기본 생성자 같은 다른 creator 없으면 jackson이 deserialize를 할 수가 없다.
+// com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of ...
+// 그래서 @NoArgsConstructor 달아줌
 public class OrderCreateRequest {
 
     private List<String> productNumbers;
