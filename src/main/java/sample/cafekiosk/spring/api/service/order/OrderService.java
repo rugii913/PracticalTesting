d17709fb9ac257cc5332feaeed122c671ee5563c
rugii913 <rugii913@gmail.com>
2023-12-22 15:58:31 +0900

    (진행 중)OrderService의 createOrder(~) 메서드에서 재고 차감 로직 추가 중, 필요한 테스트들도 추가
    - OrderService의 createOrder(~) 메서드에서 재고 차감 로직 추가 중
      - ProductType enum에서 해당 productType이 재고를 가진 품목인지 확인하는 메서드 추가
        - 이에 따른 테스트 추가
      - StockRepository에서 상품번호 리스트를 받으면 이에 맞는 Stock 모두 가져오는 메서드 추가
        - 이에 따른 테스트 추가

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -8,6 +8,8 @@ import sample.cafekiosk.spring.domain.order.Order;
 import sample.cafekiosk.spring.domain.order.OrderRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,10 +23,21 @@ public class OrderService {
 
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
+    private final StockRepository stockRepository;
 
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers(); // 요청 상품 번호 목록(중복 제거되어 있지 않음)
         List<Product> products = findProductsBy(productNumbers);
+
+        // 재고 차감 체크가 필요한 상품들 filter
+        List<String> stockProductNumbers = products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .collect(Collectors.toList()); // Product 리스트는 아니고 상품 번호 리스트 가져옴
+        // 재고 엔티티 조회
+        stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        // 상품별 counting
+        // 재고 차감 시도
 
         Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order); // PK id 받아온 order를 가져옴
