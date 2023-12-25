454f2418642d7ba052f50a428c0e51dfda6853a8
rugii913 <rugii913@gmail.com>
2023-12-25 23:19:25 +0900

    cf. 동시성 이슈가 발생할 경우, 대책 예시들

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -20,6 +20,9 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    // 원래는 동시성 이슈 고려해야되는 부분임
+    // - ex1. 동시 접근 확률이 낮은 경우 DB에서 unique index로 걸고, 충돌할 경우 재시도하게 만들기
+    // - ex2. 아예 정책을 변경해서 상품 번호를 UUID로 사용
     public ProductResponse createProduct(ProductCreateRequest request) {
         // productNumber 부여
         // DB에서 마지막 저장된 Product의 상품 번호를 읽어와서 + 1 (ex.)009 → 010
