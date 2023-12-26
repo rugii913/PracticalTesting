b56b63d44257a0a549b9ac2d3dc7c55bf24a6347
rugii913 <rugii913@gmail.com>
2023-12-26 10:58:39 +0900

    cf. 컨트롤러에서 응답을 규격화하면 메타정보 등을 추가하기 좋다
    - 공통적으로 사용할 수 있는 응답 포맷 - ApiResponse 클래스 추가
      - http status 같은 것도 명시적으로 실어서 보냄

---
+++ src/main/java/sample/cafekiosk/spring/api/ApiResponse.java
@@ -0,0 +1,27 @@
+package sample.cafekiosk.spring.api;
+
+import org.springframework.http.HttpStatus;
+import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
+
+public class ApiResponse<T> {
+
+    private int code; // HttpStatus의 값
+    private HttpStatus httpStatus;
+    private String message; // 실패 사유 등
+    private T data;
+
+    public ApiResponse(HttpStatus httpStatus, String message, T data) {
+        this.code = httpStatus.value();
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        return new ApiResponse<>(httpStatus, httpStatus.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) { // 자주 쓰이는 status마다 factory method를 만드는 것도 괜찮다.
+        return new ApiResponse<>(HttpStatus.OK, HttpStatus.OK.name(), data);
+    }
+}
