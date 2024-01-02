e7eb709ba5caaf43492b5618b343372813c87882
rugii913 <rugii913@gmail.com>
2024-01-02 17:56:48 +0900

    이 부분을 잘 mocking하는 것이 목표
    - 예외 터지는 것이 실제 메일 보내는 것이라 가정
    - mocking을 통해 실제로 메일은 발송되지 않도록 테스트하는 것이 목표

--- src/main/java/sample/cafekiosk/spring/client/mail/MailSendClient.java
+++ src/main/java/sample/cafekiosk/spring/client/mail/MailSendClient.java
@@ -9,6 +9,8 @@ public class MailSendClient {
     public boolean sendEmail(String fromEmail, String toEmail, String subject, String content) {
         // 메일 전송
         log.info("메일 전송");
-        return true;
+        throw new IllegalArgumentException("메일 전송");
+        // 이렇게 예외를 던지는 것이 실제 메일을 보내는 것이라 가정
+        // - mock을 이용해서 실제로 메일이 발송되지 않게(예외가 터지지 않게) 하려고 한다.
     }
 }
